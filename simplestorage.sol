7ab190ab7022773393869d02c7c180baf31d9c49
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2021-12-22 13:46:32 -0800

    Create simplestorage.sol

---
+++ simplestorage.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    
+    // this will get initialized to 0!
+    uint256 favoriteNumber;
+    bool favoriteBool;
+    
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    People[] public people;
+    //array named as people of type struct people.
+    
+    mapping(string => uint256) public nameToFavoriteNumber;
+    //Each string is mapped with unsigned integer. Integer is passed through the store function
+    
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+    //To retrieve the favorite number.
+    
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }    
+    //Add a person and favorite number.
+    
+}
